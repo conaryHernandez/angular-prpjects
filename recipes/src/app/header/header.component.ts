b83718e9dfa93827dd7ed909d5f34da8ced58378
Conary <31629328+conaryHernandez@users.noreply.github.com>
2020-06-04 18:19:34 -0600

    recipes
    displaying recipe detail

--- recipes/src/app/header/header.component.ts
+++ recipes/src/app/header/header.component.ts
@@ -1,9 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
 })
 export class Header {
+  @Output() featureSelected = new EventEmitter<string>();
+
   collapsed = true;
+
+  onSelect(feature: string) {
+    this.featureSelected.emit(feature);
+  }
 }
--- recipes/src/app/recipes/recipe-list/recipe-list.component.ts
+++ recipes/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -14,8 +14,13 @@ export class RecipeListComponent implements OnInit {
       'https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2013/05/chorizo-mozarella-gnocchi-bake-cropped.jpg'
     ),
   ];
+  @Output() onRecipeWasSelected = new EventEmitter<Recipe>();
 
   constructor() {}
 
   ngOnInit(): void {}
+
+  onRecipeSelected(recipe) {
+    this.onRecipeWasSelected.emit(recipe);
+  }
 }
