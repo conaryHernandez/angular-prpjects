a2bd37989c4685aef310e15ddd4a9d548772f934
Conary <31629328+conaryHernandez@users.noreply.github.com>
2020-06-08 20:56:07 -0600

    routing
    programatically navigation

--- routing/src/app/home/home.component.ts
+++ routing/src/app/home/home.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Router, ActivatedRoute } from "@angular/router";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.css"],
 })
 export class HomeComponent implements OnInit {
+  constructor(private router: Router, private route: ActivatedRoute) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  loadServers(id) {
+    // this.router.navigate(["/servers"], { relativeTo: this.route });
+
+    this.router.navigate(["/servers", id, "edit"], {
+      queryParams: { allowEdit: "1" },
+      fragment: "loading",
+    });
   }
-
 }
