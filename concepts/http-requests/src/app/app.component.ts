c530e7d4b0cca28e1d02a8816d0ddaca196438dc
Conary <31629328+conaryHernandez@users.noreply.github.com>
2020-06-22 16:25:02 -0600

    http requests
    initial files and post request

---
+++ concepts/http-requests/src/app/app.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+
+@Component({
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
+})
+export class AppComponent implements OnInit {
+  loadedPosts = [];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.fetchPosts();
+  }
+
+  onCreatePost(postData: { title: string; content: string }) {
+    // Send Http request
+    this.http
+      .post(
+        "https://catch-of-the-day-conary.firebaseio.com/posts.json",
+        postData
+      )
+      .subscribe((responseData) => {
+        console.log(responseData);
+      });
+  }
+
+  onFetchPosts() {
+    this.fetchPosts();
+  }
+
+  onClearPosts() {
+    // Send Http request
+  }
+
+  private fetchPosts() {
+    this.http
+      .get("https://catch-of-the-day-conary.firebaseio.com/posts.json")
+      .subscribe((posts) => {
+        console.log("posts", posts);
+      });
+  }
+}
