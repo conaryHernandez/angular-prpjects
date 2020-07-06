1cf14fff806c3d33a6ce6f9194fc0a15ed5d0e4e
Conary <31629328+conaryHernandez@users.noreply.github.com>
2020-07-06 15:52:23 -0600

    ngrx
    fetching recipes

--- recipes-ngrx/src/app/app.module.ts
+++ recipes-ngrx/src/app/app.module.ts
@@ -15,6 +15,7 @@ import { CoreModule } from './core.module';
 import * as fromApp from './store/app.reducer';
 import { AuthEffects } from './auth/store/auth.effects';
 import { environment } from '../environments/environment';
+import { RecipeEffects } from './recipes/store/recipe.effects';
 
 @NgModule({
   declarations: [AppComponent, Header],
@@ -23,7 +24,7 @@ import { environment } from '../environments/environment';
     AppRoutingModule,
     HttpClientModule,
     StoreModule.forRoot(fromApp.appReducer),
-    EffectsModule.forRoot([AuthEffects]),
+    EffectsModule.forRoot([AuthEffects, RecipeEffects]),
     SharedModule,
     CoreModule,
     StoreDevtoolsModule.instrument({ logOnly: environment.production }),
